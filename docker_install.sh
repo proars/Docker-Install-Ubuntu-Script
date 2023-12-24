6728cc593547265c4ca64f6159ebc128d8e8d07f
Ars <96272922+proars@users.noreply.github.com>
2023-12-24 12:33:01 -0800

    Update docker_install.sh

--- docker_install.sh
+++ docker_install.sh
@@ -3,18 +3,16 @@
 ###################################################
 
 #!/bin/bash
-sudo apt-get -y remove docker docker-engine docker.io containerd runc
+#sudo apt-get -y remove docker docker-engine docker.io containerd runc
 sudo apt-get update
-sudo apt-get install ca-certificates curl gnupg
+sudo apt-get install -y ca-certificates curl gnupg lsb-release curl
+sudo mkdir -p /etc/apt/keyrings
 echo "Add Docker’s official GPG key:"
-echo \
-  "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
-  "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
-  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-  
-echo "Install Docker latest version"
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null  
+sudo chmod a+r /etc/apt/keyrings/docker.gpg
 sudo apt-get update
-sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
-
+echo "Install Docker latest version"
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-compose-plugin
 
 
